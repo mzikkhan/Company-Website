2f18b184d991991b48cb5ae511d13b3c9290ad44
Zaed Khan <zaedkhan2019@gmail.com>
2024-01-27 15:53:07 +0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout='wide')
+
+st.header("The Best Company")
+
+content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+st.write(content)
+
+st.header("Our Team")
+
+df = pd.read_csv("data.csv")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    for index, row in df[:4].iterrows():
+        st.header(f'{row["first name"].capitalize()} {row["last name"].capitalize()}')
+        st.write(row['role'])
+        st.image('images/' + row['image'])
+
+with col2:
+    for index, row in df[4:8].iterrows():
+        st.header(f'{row["first name"].capitalize()} {row["last name"].capitalize()}')
+        st.write(row['role'])
+        st.image('images/' + row['image'])
+
+with col3:
+    for index, row in df[8:].iterrows():
+        st.header(f'{row["first name"].capitalize()} {row["last name"].capitalize()}')
+        st.write(row['role'])
+        st.image('images/' + row['image'])
