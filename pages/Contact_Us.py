5df3f535d0e2c42897734ceb8f495bf04c19fc46
Zaed Khan <zaedkhan2019@gmail.com>
2024-01-28 09:41:35 +0600

    Integrated email functionality and dropdown menu

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,14 +1,20 @@
 import streamlit as st
 from send_email import send_email
+import pandas as pd
 
 st.header("Contact Me")
 
+df = pd.read_csv('topics.csv')
+
 with st.form(key="email_form"):
     user_email = st.text_input("Your Email Address: ")
-    raw_message = st.text_area("Your message: ")
+    topic = st.selectbox('What topic do you want to discuss?',
+                         df['topic'])
+    raw_message = st.text_area("Text: ")
     message = f"""
 Subject: New email from {user_email}
-from: {user_email}
+From: {user_email}
+Topic: {topic}
 {raw_message}"""
     button = st.form_submit_button()
     if button:
